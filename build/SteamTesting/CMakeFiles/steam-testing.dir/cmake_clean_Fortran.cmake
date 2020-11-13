2c9a73a5cc8cb8b2661532abd596ba0da6c3f380
Panos Asproulis <panos.asproulis@gmail.com>
2020-11-13 05:42:35 +0100

    Remove build files

--- build/SteamTesting/CMakeFiles/steam-testing.dir/cmake_clean_Fortran.cmake
+++
@@ -1,6 +0,0 @@
-# Remove fortran modules provided by this target.
-FILE(REMOVE
-  "steamtablestesting.mod"
-  "STEAMTABLESTESTING.mod"
-  "CMakeFiles/steam-testing.dir/steamtablestesting.mod.stamp"
-  )
